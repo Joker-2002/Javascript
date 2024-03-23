a88ad1353a68bcbcdabda08bfc84f2831e1bbf04
Debasish Kumar <101468860+Joker-2002@users.noreply.github.com>
2024-03-23 11:54:51 +0000

    conversion of datatypes

---
+++ basics/04_conversionOperation.js
@@ -0,0 +1,20 @@
+// let score = "33"
+// console.log(typeof(score))
+
+// let value = Number(score)
+// console.log(typeof(value))
+
+let s = "33asbv"
+console.log(s)
+console.log(typeof(s))
+
+let val = Number(s)
+console.log(val)
+console.log(typeof(val))
+
+
+//"33" => 33 ; string to number conversion
+// "233dds" => NaN
+// null => 0
+// undefined => NaN
+// 1,0 => True,False

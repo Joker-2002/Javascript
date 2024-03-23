94f1004c629123098a11c8e1cf4b248d66f2d48b
Debasish Kumar <101468860+Joker-2002@users.noreply.github.com>
2024-03-23 17:00:31 +0000

    operation

--- basics/04_conversionOperation.js
+++ basics/04_conversionOperation.js
@@ -1,16 +1,15 @@
 // let score = "33"
 // console.log(typeof(score))
 
-// let value = Number(score)
-// console.log(typeof(value))
+//cc
 
-let s = "33asbv"
-console.log(s)
-console.log(typeof(s))
+// let s = "33asbv"
+// console.log(s)
+// console.log(typeof(s))
 
-let val = Number(s)
-console.log(val)
-console.log(typeof(val))
+// let val = Number(s)
+// console.log(val)
+// console.log(typeof(val))
 
 
 //"33" => 33 ; string to number conversion
@@ -18,3 +17,41 @@ console.log(typeof(val))
 // null => 0
 // undefined => NaN
 // 1,0 => True,False
+
+
+// let A = undefined
+// console.log(typeof(A))
+
+
+// ---------------------------------------------------------------------------------------
+//OPERATIONS
+
+// let value = 3 
+// let negval = -value
+
+// console.log(negval);
+
+/*  Simple Arithematic calculation, String concatenation
+increment decrement
+ */
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+/*string and number addition. first part string then all operators converts to string
+ and if last operator is string first operators behaves as number and last operator as string.*/
+
+
+//--------------------------------------------------------------------------------------------
+/* COMPARISONS
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+these are simple which gives output as true or false.
+
+
